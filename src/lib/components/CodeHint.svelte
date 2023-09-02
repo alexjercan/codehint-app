04464bee78cb002ee952c6ce4cc9a1af0374a294
Alex Jercan <jercan_alex27@yahoo.com>
2023-09-02 08:37:52 +0300

    feat: added text editor to codehint

--- src/lib/components/CodeHint.svelte
+++ src/lib/components/CodeHint.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	let code = "";
-	let model = "openai";
-	let result = "";
+	import { onMount } from "svelte";
 
-	async function handleSubmit() {
+	let editor = null;
+	let model = "openai";
+	let hint: Promise<string> = Promise.resolve("");
+
+	async function getHint() {
+		let code = editor.getValue();
+
 		const response = await fetch("/api/hint", {
 			method: "POST",
 			headers: {
@@ -11,9 +15,30 @@
 			},
 			body: JSON.stringify({ code, model })
 		});
+		const result = await response.text();
 
-		result = await response.text();
+		if (response.ok) {
+			return result;
+		} else {
+			throw new Error(result);
+		}
 	}
+
+	function handleSubmit() {
+		hint = getHint();
+	}
+
+	onMount(async () => {
+		const ace = await import("ace-builds/src-noconflict/ace");
+		await import("ace-builds/esm-resolver");
+
+		editor = ace.edit("editor");
+		editor.session.setMode("ace/mode/python");
+		editor.setTheme("ace/theme/dracula");
+		editor.setOptions({
+			fontSize: "16pt"
+		});
+	});
 </script>
 
 <div class="w-full h-full flex flex-col justify-start items-center space-y-4">
@@ -24,13 +49,23 @@
 	</select>
 
 	<label for="code">Enter Code:</label>
-	<textarea
-		id="code"
-		class="textarea textarea-bordered textarea-lg w-full max-w-xs"
-		bind:value={code}
-	/>
+	<div id="editor" />
 
 	<button class="btn" on:click={handleSubmit}>Submit</button>
 
-	<div>{result}</div>
+	{#await hint}
+		<p>Waiting for hint...</p>
+	{:then hint}
+		<p>{hint}</p>
+	{:catch error}
+		<p class="text-red-500">{error.message}</p>
+	{/await}
 </div>
+
+<style>
+	#editor {
+		position: relative;
+		width: 500px;
+		height: 400px;
+	}
+</style>
