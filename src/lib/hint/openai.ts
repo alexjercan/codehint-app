3822a30d002c9ace170e501219c787074949d661
Alex Jercan <jercan_alex27@yahoo.com>
2023-09-07 15:16:13 +0300

    what

--- src/lib/hint/openai.ts
+++ src/lib/hint/openai.ts
@@ -70,5 +70,8 @@ export async function generate(code: string): Promise<Hint> {
 		model: "gpt-3.5-turbo"
 	});
 
-	return await JSON.parse(completion.choices[0].message.content ?? "");
+    const content = completion.choices[0].message.content ?? "";
+    console.log(content);
+
+	return await JSON.parse(content);
 }
